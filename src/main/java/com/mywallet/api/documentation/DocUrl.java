f3769fe08e8cb83da63e66fdb2f09f85c5156ccb
MCPutro <em.chepe@gmail.com>
2022-01-24 23:06:31 +0700

    add @ApiIgnore in url document

--- src/main/java/com/mywallet/api/documentation/DocUrl.java
+++ src/main/java/com/mywallet/api/documentation/DocUrl.java
@@ -1,10 +1,14 @@
 package com.mywallet.api.documentation;
 
+import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.view.RedirectView;
+import springfox.documentation.annotations.ApiIgnore;
 
+
+@ApiIgnore
 @RestController
 public class DocUrl {
 
