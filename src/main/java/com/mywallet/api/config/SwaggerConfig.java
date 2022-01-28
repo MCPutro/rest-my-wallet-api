c481c7128516214e1a1a6ff8b43fffc1a69f12b9
MCPutro <em.chepe@gmail.com>
2022-01-28 22:08:27 +0700

    update swagger

--- src/main/java/com/mywallet/api/config/SwaggerConfig.java
+++ src/main/java/com/mywallet/api/config/SwaggerConfig.java
@@ -36,7 +36,7 @@ public class SwaggerConfig {
         return new ApiInfo(
                 "My Wallet REST API",
                 "Product API to perform CRUD operations",
-                "2.0.6",
+                "2.0.7",
                 null,//"Terms of service",
                 new Contact(
                         "Mu'ti C Putro",
